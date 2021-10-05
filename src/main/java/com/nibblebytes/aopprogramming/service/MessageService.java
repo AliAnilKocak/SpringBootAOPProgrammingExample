3eca4694173ae15fc3c63f291de73a0b2bc71cde
AliAnilKocak <you@example.com>
2021-10-05 17:08:54 +0300

    edit message

--- src/main/java/com/nibblebytes/aopprogramming/service/MessageService.java
+++ src/main/java/com/nibblebytes/aopprogramming/service/MessageService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class MessageService {
     public String receiveMessage(String message) {
-        System.out.println("Method receive message1 param : " + message);
+        System.out.println("Method receive message1 param  : " + message);
         return message;
     }
 }
